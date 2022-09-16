75dcfd0d32c09c7fdcb170e6ece7e285157707d6
Pushy <support@pushy.me>
2022-09-16 09:40:51 +0000

    Pushy.toggleMethodSwizzling(): expose all UIApplicationDelegate and UNUserNotificationCenterDelegate methods

--- ios/PushyModule.h
+++ ios/PushyModule.h
@@ -2,15 +2,7 @@
 #import <React/RCTEventEmitter.h>
 #import <UserNotifications/UserNotifications.h>
 
-@interface PushyObj : NSObject
-- (void)application:(UIApplication * _Nonnull)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData * _Nonnull)deviceToken;
-- (void)application:(UIApplication * _Nonnull)application didFailToRegisterForRemoteNotificationsWithError:(NSError * _Nonnull)error;
-- (void)application:(UIApplication * _Nonnull)application didReceiveRemoteNotification:(NSDictionary * _Nonnull)userInfo;
-- (void)application:(UIApplication * _Nonnull)application didReceiveRemoteNotification:(NSDictionary * _Nonnull)userInfo fetchCompletionHandler:(void (^ _Nonnull)(UIBackgroundFetchResult))completionHandler;
-- (void)userNotificationCenter:(UNUserNotificationCenter* _Nonnull)center
-       willPresentNotification:(UNNotification* _Nonnull)notification
-         withCompletionHandler:(void (^_Nonnull)
-       (UNNotificationPresentationOptions options))completionHandler;
+@interface PushyObj : NSObject <UIApplicationDelegate, UNUserNotificationCenterDelegate>
 @end
 
 @interface PushyModule : RCTEventEmitter <RCTBridgeModule>
