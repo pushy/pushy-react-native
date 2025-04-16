6597b03f4a46313a66e80d104ac6a71c083eb7ee
Pushy <support@pushy.me>
2025-04-16 12:19:22 -0300

    PushyNotificationService.java: Add compatibility with RN 0.79.0 (Fixes #103)

--- android/src/main/java/me/pushy/sdk/react/services/PushyNotificationService.java
+++ android/src/main/java/me/pushy/sdk/react/services/PushyNotificationService.java
@@ -1,39 +1,56 @@
 package me.pushy.sdk.react.services;
 
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.facebook.react.HeadlessJsTaskService;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactHost;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.bridge.Arguments;
 
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
-import java.util.Objects;
+import java.lang.reflect.Field;
 
 import me.pushy.sdk.react.config.PushyHeadlessJSConfig;
+import me.pushy.sdk.util.PushyLogger;
 
 public class PushyNotificationService extends HeadlessJsTaskService {
-
+    @SuppressLint("DiscouragedPrivateApi")
     public PushyNotificationService(Context context) {
         super();
 
         // Inject synthetic context
         attachBaseContext(context);
-    }
 
-    @Override
-    protected ReactNativeHost getReactNativeHost() {
-        // Override implementation as original getReactNativeHost throws NullReferenceException
-        return ((ReactApplication) getApplicationContext()).getReactNativeHost();
-    }
+        // Get Application context object
+        Application app = (Application) context.getApplicationContext();
 
-    protected ReactHost getReactHost() {
-        return ((ReactApplication) getApplicationContext()).getReactHost();
+        // Use reflection to override the private mApplication field in Service superclass
+        // To resolve NullPointerException errors when launching the service directly using onStartCommand()
+        Field field;
+
+        try {
+            // Get privately declared field
+            field = Service.class.getDeclaredField("mApplication");
+        } catch (NoSuchFieldException e) {
+            // Log exception
+            PushyLogger.e("Failed to fetch declared field mApplication of Service superclass", e);
+            return;
+        }
+
+        // Make it accessible
+        field.setAccessible(true);
+
+        try {
+            // Set a new Application value
+            field.set(this, app);
+        } catch (IllegalAccessException e) {
+            // Log exception
+            PushyLogger.e("Failed to override mApplication field of Service superclass", e);
+        }
     }
 
     @Override
